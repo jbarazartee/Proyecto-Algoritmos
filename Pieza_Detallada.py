12add0fd4095a2f2d63adc7f8064ae5e841a9052
SofiaMachado1 <smachado@correo.unimet.edu.ve>
2025-08-07 19:28:37 -0400

    Update Pieza_Detallada.py
    errores de indentacion arreglados, cami haz los otros

--- Pieza_Detallada.py
+++ Pieza_Detallada.py
@@ -2,7 +2,7 @@ class Pieza_Detallada:
     """
     Describe una pieza artística con todos sus detalles relevantes: título, autor, nacionalidad, fechas, tipo, año y enlace a imagen.
     """
-   def __init__(self, codigo, titulo, autor, origen, nacimiento, fallecimiento, tipo, anio, url_imagen):
+    def __init__(self, codigo, titulo, autor, origen, nacimiento, fallecimiento, tipo, anio, url_imagen):
         self.codigo = codigo
         self.titulo = titulo
         self.autor = autor
@@ -12,13 +12,13 @@ class Pieza_Detallada:
         self.tipo = tipo
         self.anio = anio
         self.url_imagen = url_imagen
+        
+    def resumen (self):
+        """
+        Escribe la informacion basica de la pieza: codigo, titulo y autor 
 
-   def resumen (self):
-       """
-       Escribe la informacion basica de la pieza: codigo, titulo y autor 
-
-       Returns: Un str con el formato "codigo - titulo (autor)"
-       """
-       return f"{self.codigo} - {self.titulo} ({self.autor})"
+        Returns: Un str con el formato "codigo - titulo (autor)"
+        """
+        return f"{self.codigo} - {self.titulo} ({self.autor})"
 
 
