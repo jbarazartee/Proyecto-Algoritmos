3101a6c970c65bdad74f388113c1226d54a60a85
SofiaMachado1 <smachado@correo.unimet.edu.ve>
2025-08-06 23:00:17 -0400

    Menu.py
    esta es la estructura basica del menu

---
+++ Menu.py
@@ -0,0 +1,24 @@
+def mostrar_menu():
+
+  while True:
+    print("\n=== Menu Principal ===")
+    print("1. Ver obras por departamento")
+    print("2. Ver obras por nacionalidad de autor")
+    print("3. Ver obras por nombre de autor")
+    print("4. Ver detalles de una obra")
+    print("5. Salir")
+    opcion = input("Seleccione una opcion: ")
+    if opcion == "1":
+      
+    elif opcion == "2":
+      
+    elif opcion == "3":
+      nombre = input("Ingrese nombre del autor: ")
+    elif opcion == "4":
+      
+    elif opcion == "5":
+      print("Hasta luego")
+      break
+    else:
+      print("Opcion no valida")
+    
