0a1637ad54b36051a0a1428d575bc48df4364377
SofiaMachado1 <smachado@correo.unimet.edu.ve>
2025-08-07 17:59:09 -0400

    Update Menu.py
    menu ya hecho, revisen por favor

--- Menu.py
+++ Menu.py
@@ -1,6 +1,12 @@
+from solicitudes_api import ConsultasMuseo
+
 def mostrar_menu():
 
-  while True:
+  """
+    Muestra el menú principal y gestiona la navegación del usuario.
+    """
+    consultas = ConsultasMuseo()
+    while True:
     print("\n=== Menu Principal ===")
     print("1. Ver obras por departamento")
     print("2. Ver obras por nacionalidad de autor")
@@ -9,13 +15,27 @@ def mostrar_menu():
     print("5. Salir")
     opcion = input("Seleccione una opcion: ")
     if opcion == "1":
-      
+        consultas.mostrar_departamentos()
+            try:
+                dep_id = int(input("Ingrese el ID del departamento: "))
+                consultas.buscar_por_departamento(dep_id)
+            except Exception:
+                print("ID inválido.")
+              
     elif opcion == "2":
+      consultas.buscar_por_nacionalidad()
       
     elif opcion == "3":
       nombre = input("Ingrese nombre del autor: ")
-    elif opcion == "4":
+      consultas.buscar_por_autor(nombre)
       
+    elif opcion == "4":
+        try:
+                obra_id = int(input("Ingrese el ID de la obra: "))
+                consultas.detalles_pieza(obra_id)
+            except Exception:
+                print("ID inválido.")
+              
     elif opcion == "5":
       print("Hasta luego")
       break
