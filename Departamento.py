4c5735eaca68143394aba75ab41e95af6cde26c2
CamilaSego007 <camila.segovia@correo.unimet.edu.ve>
2025-08-06 23:25:36 -0400

    Update Departamento.py

--- Departamento.py
+++ Departamento.py
@@ -4,4 +4,4 @@ def __init__(self, id_departamento, nombre_departamento):
         self.id_departamento = id_departamento
         self.nombre_departamento = nombre_departamento
 
-#falta el metodo que devuelva el departamento con id y nombre
+
