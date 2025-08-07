d0597d59c6ecdf5be4ff1caa99071c97a5528650
jbarazartee <jbarazarte@correo.unimet.edu.ve>
2025-08-06 23:42:26 -0400

    Departamento.py
    listo el objeto Departamento por ahora

--- Departamento.py
+++ Departamento.py
@@ -1,7 +1,9 @@
 #Módulo que define las entidades principales del sistema de gestión de piezas artísticas y áreas del museo.
+class Departamento:
+        
+        def __init__(self, id_departamento, nombre_departamento):
+                self.id_departamento = id_departamento
+                self.nombre_departamento = nombre_departamento
 
-def __init__(self, id_departamento, nombre_departamento):
-        self.id_departamento = id_departamento
-        self.nombre_departamento = nombre_departamento
-
-
+        def descripcion(self):
+                return f"{self.id_departamento} - {self.nombre_departamento}"
