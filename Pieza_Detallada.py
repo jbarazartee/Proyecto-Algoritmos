9d3ab0ac4525b2eb5c081713c2cdcae49e4c834c
jbarazartee <jbarazarte@correo.unimet.edu.ve>
2025-08-07 11:53:10 -0400

    Update Pieza_Detallada.py
    documentado en docstring

--- Pieza_Detallada.py
+++ Pieza_Detallada.py
@@ -1,7 +1,7 @@
 class Pieza_Detallada
-
-    #Describe una pieza artística con todos sus detalles relevantes: título, autor, nacionalidad, fechas, tipo, año y enlace a imagen.
-    
+    """
+    Describe una pieza artística con todos sus detalles relevantes: título, autor, nacionalidad, fechas, tipo, año y enlace a imagen.
+    """
    def __init__(self, codigo, titulo, autor, origen, nacimiento, fallecimiento, tipo, anio, url_imagen):
         self.codigo = codigo
         self.titulo = titulo
@@ -14,7 +14,11 @@ class Pieza_Detallada
         self.url_imagen = url_imagen
 
    def resumen (self):
-      #Escribe la informacion basica de la pieza: codigo, titulo y autor 
+       """
+       Escribe la informacion basica de la pieza: codigo, titulo y autor 
+
+       Returns: Un str con el formato "codigo - titulo (autor)"
+       """
        return f"{self.codigo} - {self.titulo} ({self.autor})"
 
 
