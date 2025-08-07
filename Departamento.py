644bb59d9ff5d39bb02c0fd39cfc8f028d755c80
jbarazartee <jbarazarte@correo.unimet.edu.ve>
2025-08-06 21:00:07 -0400

    clase Departamento.py

---
+++ Departamento.py
@@ -0,0 +1,7 @@
+class Departamento:
+
+def __init__(self, id_departamento, nombre_departamento):
+        self.id_departamento = id_departamento
+        self.nombre_departamento = nombre_departamento
+
+#falta el metodo que devuelva el departamento con id y nombre
