10a5a0c7bc6027ca9ef10b00f38c3fee0cb0021d
jbarazartee <jbarazarte@correo.unimet.edu.ve>
2025-08-07 12:09:40 -0400

    Update Departamento.py
    agregue docstring

--- Departamento.py
+++ Departamento.py
@@ -1,4 +1,6 @@
-#Módulo que define las entidades principales del sistema de gestión de piezas artísticas y áreas del museo.
+"""
+Módulo que define las entidades principales del sistema de gestión de piezas artísticas y áreas del museo.
+"""
 class Departamento:
         
         def __init__(self, id_departamento, nombre_departamento):
@@ -6,4 +8,7 @@ class Departamento:
                 self.nombre_departamento = nombre_departamento
 
         def descripcion(self):
+                """
+                Devuelve una cadena que tiene id - nombre
+                """
                 return f"{self.id_departamento} - {self.nombre_departamento}"
