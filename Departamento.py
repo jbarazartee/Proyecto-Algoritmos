cce2f2b2e9d1a58069e2eacd2b76a60af1a39ff1
CamilaSego007 <camila.segovia@correo.unimet.edu.ve>
2025-08-07 22:27:57 -0400

    Update Departamento.py
    tuvimos aqui error de indentacion, ya lo corregi

--- Departamento.py
+++ Departamento.py
@@ -3,12 +3,12 @@ Módulo que define las entidades principales del sistema de gestión de piezas a
 """
 class Departamento:
         
-        def __init__(self, id_departamento, nombre_departamento):
-                self.id_departamento = id_departamento
-                self.nombre_departamento = nombre_departamento
+    def __init__(self, id_departamento, nombre_departamento):
+        self.id_departamento = id_departamento
+        self.nombre_departamento = nombre_departamento
 
-        def descripcion(self):
-                """
-                Devuelve una cadena que tiene id - nombre
-                """
-                return f"{self.id_departamento} - {self.nombre_departamento}"
+    def descripcion(self):
+        """
+        Devuelve una cadena que tiene id - nombre
+        """
+        return f"{self.id_departamento} - {self.nombre_departamento}"
